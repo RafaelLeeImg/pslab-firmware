ecc77a260277788b91c5d6ace1709cc20bfa9b00
Rafael Lee <rafaellee.img@gmail.com>
2022-03-02 15:40:50 +0800

    refactor: convert DAC driver with SPI bus

--- pslab-core.X/instruments/powersource.c
+++ pslab-core.X/instruments/powersource.c
@@ -2,18 +2,52 @@
 #include "../commands.h"
 #include "../bus/uart/uart1.h"
 #include "../bus/i2c/i2c.h"
+#include "../bus/spi/spi_driver.h"
 #include "../helpers/delay.h"
 #include "../registers/system/pin_manager.h"
 #include "powersource.h"
 
+extern void SetCS(uint8_t channel);
+extern void UnsetCS();
+
+
+#define MCP4822_CHANNEL_A 0
+#define MCP4822_CHANNEL_B 0x8000
+
+#define MCP4822_GAIN_1 0
+#define MCP4822_GAIN_2 0x2000
+
+#define MCP4822_ACTIVE 0x1000
+#define MCP4822_SHUTDOWN 0
+
+#define CS_CHANNEL_3 3 // CS3 linked to A2 in 74HC4051
+#define CS_CHANNEL_4 4
+
+static uint16_t channel_mapping_table[4][2] = {
+    {CS_CHANNEL_3, MCP4822_CHANNEL_A},
+    {CS_CHANNEL_3, MCP4822_CHANNEL_B},
+    {CS_CHANNEL_4, MCP4822_CHANNEL_A},
+    {CS_CHANNEL_4, MCP4822_CHANNEL_B}
+};
+
+
 response_t POWER_SOURCE_SetPower(void) {
+    // the uart read need to be at the top of the function
+    // since the data is sending continuously.
+    uint8_t channel = UART1_Read() & 0x03;
+    uint16_t power = UART1_ReadInt() & 0xFFF;
+    // the 2D array is 16 bits
+    uint8_t cs_pin = 0xFF & channel_mapping_table[channel][0];
+    uint16_t mcp4822_channel = channel_mapping_table[channel][1];
+    uint16_t send_data = mcp4822_channel|MCP4822_ACTIVE|MCP4822_GAIN_1|power;
+
     SPI_DRIVER_Close();
-    SPI_DRIVER_Open(PGA_CONFIG);
-    SetCS(channel);
-    int ret = SPI_DRIVER_ExchangeByte(max5400_resistor);
-    SetCS(7); // select channel 7 which is not connected to any device
-    SPI_DRIVER_Close();
-    return ret;
+    SPI_DRIVER_Open(DAC_CONFIG);
+    SetCS(cs_pin);
+    SPI_DRIVER_ExchangeWord(send_data);
+    UnsetCS();
+
+    return SUCCESS;
 }
 
 response_t POWER_SOURCE_SetDAC(void) {
